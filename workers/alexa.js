12e8b996ead7e258b5e3a4f8851f1e9231609a53
Jason Chen <jhchen7@gmail.com>
2015-06-24 11:13:56 -0700

    add real progress bars

--- workers/alexa.js
+++ workers/alexa.js
@@ -15,13 +15,16 @@ var SOURCE_URL = 'https://dl.dropboxusercontent.com/u/796228/top-100.csv.zip';
 
 // 12 hours ago to compensate for possible timezone misconfigurations
 var updatedAt = new Date(Date.now() - (60 * 60 * 12 * 1000));
-var numParsed = 0;
 
 var pass = new stream.PassThrough();
 var parser = parse();
 var queue = async.priorityQueue(work, os.cpus().length);
 queue.drain = drain;
-var progress = setInterval(showProgress, 1000);
+var bar = new Progress('Importing [:bar] :percent :etas ', {
+  incomplete: ' ',
+  total: 1000000,
+  width: 40
+});
 
 pass.pipe(parser);
 http.get(SOURCE_URL, getList);
@@ -39,7 +42,6 @@ parser.on('finish', function(d) {
 function drain() {
   bookshelf.knex('sites').where('updated_at', '<', updatedAt).update({ rank: null }).finally(function() {
     bookshelf.knex.destroy();
-    clearInterval(progress);
   });
 };
 
@@ -49,16 +51,11 @@ function getList(response) {
   });
 }
 
-function showProgress() {
-  console.log('Queue:', queue.length(), 'Parsed:', numParsed);
-}
-
 function work(task, callback) {
   if (typeof task === 'string') {
     // Read records
     var record;
     while (record = parser.read()) {
-      numParsed += 1;
       queue.push({ rank: record[0], domain: record[1] }, HIGH_PRIORITY);
     }
     callback(null);
@@ -66,6 +63,7 @@ function work(task, callback) {
     // Insert records
     // TODO use upsert when Posgres 9.5 comes out
     bookshelf.knex('sites').count('domain').where({ domain: task.domain }).then(function(rows) {
+      bar.tick();
       if (rows.length > 0 && rows[0].count > 0) {
         return bookshelf.knex('sites')
           .where({ domain: task.domain })
--- workers/history.js
+++ workers/history.js
@@ -3,26 +3,30 @@
 var async = require('async');
 var bookshelf = require('../db/bookshelf');
 var os = require('os');
+var Progress = require('progress');
 
-var createdAt = new Date();  // Hold creation time constant
+var CREATED_AT = new Date();  // Hold creation time constant
 
-async.waterfall([
-  function(callback) {
-    bookshelf.knex('libraries').select('id').then(callback.bind(callback, null));
-  },
-  function(rows, callback) {
-    async.eachLimit(rows, os.cpus().length, function(row, callback) {
-      var select = bookshelf.knex('libraries_sites').select('site_id').where({ library_id: row.id }).toString();
-      bookshelf.knex('histories').insert({
-        library_id: row.id,
-        sites: bookshelf.knex.raw('array(' + select + ')'),
-        created_at: createdAt
-      }).then(callback.bind(callback, null), function(err) {
-        console.error('Error inserting:', row, err);
-        callback(null);
-      });
-    }, callback);
-  },
-], function(err) {
-  bookshelf.knex.destroy();
+bookshelf.knex('libraries').select('id').then(function(rows) {
+  var bar = new Progress('Calculating [:bar] :percent :etas ', {
+    incomplete: ' ',
+    total: rows.length,
+    width: 40
+  });
+  async.eachLimit(rows, os.cpus().length, function(row, callback) {
+    bar.tick();
+    var select = bookshelf.knex('libraries_sites').select('site_id').where({ library_id: row.id }).toString();
+    var count = bookshelf.knex('libraries_sites').count('*').where({ library_id: row.id }).toString();
+    bookshelf.knex('histories').insert({
+      library_id: row.id,
+      count: bookshelf.knex.raw('COALESCE((' + count + '), 0)'),
+      sites: bookshelf.knex.raw('array(' + select + ')'),
+      created_at: CREATED_AT
+    }).then(callback.bind(callback, null), function(err) {
+      console.error('Error inserting:', row, err);
+      callback(null);
+    });
+  }, function(err) {
+    bookshelf.knex.destroy();
+  });
 });
