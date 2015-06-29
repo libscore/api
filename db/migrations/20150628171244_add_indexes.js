888ace55a39e30579c0c1f2e9015e2184a86be6e
Jason Chen <jhchen7@gmail.com>
2015-06-28 17:29:15 -0700

    add more indexes

---
+++ db/migrations/20150628171244_add_indexes.js
@@ -0,0 +1,29 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('histories', function(table) {
+      table.index('count');
+      table.index('created_at');
+    }),
+    knex.schema.table('libraries_sites', function(table) {
+      table.index('updated_at');
+    }),
+    knex.schema.table('sites', function(table) {
+      table.index('updated_at');
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('histories', function(table) {
+      table.dropIndex('count');
+      table.dropIndex('created_at');
+    }),
+    knex.schema.table('libraries_sites', function(table) {
+      table.dropIndex('updated_at');
+    }),
+    knex.schema.table('sites', function(table) {
+      table.dropIndex('updated_at');
+    })
+  ]);
+};
