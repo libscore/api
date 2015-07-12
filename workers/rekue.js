5666304d1d1b5c05f838bcd90ee07b195219195f
Jason Chen <jhchen7@gmail.com>
2015-07-12 11:02:00 -0700

    add missing callbacks

--- workers/rekue.js
+++ workers/rekue.js
@@ -5,9 +5,10 @@ var queue = kue.createQueue({
   redis: { auth: process.env.LIBSCORE_REDIS_PASS }
 });
 queue.failed(function(err, ids) {
+  console.log('Found', ids.length, 'jobs');
   async.eachLimit(ids, 10, function(id, callback) {
     kue.Job.get(id, function(err, job) {
-      if (err || !job) return;
+      if (err || !job) return callback(null);
       job.data.priority += 1;
       if (job.data.priority < 3) {
         var newJob = queue.create('website', job.data).priority(job.data.priority).ttl(60*1000);
@@ -15,6 +16,8 @@ queue.failed(function(err, ids) {
           job.remove();
           newJob.save(callback);
         });
+      } else {
+        callback(null);
       }
     });
   })
