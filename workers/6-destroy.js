0111451885533aadce47f3e5888adf08476101a8
Jason Chen <jhchen7@gmail.com>
2015-08-03 21:33:08 -0700

    fix paging on destroy

--- workers/6-destroy.js
+++ workers/6-destroy.js
@@ -5,6 +5,17 @@ var api = new DigitalOcean(process.env.LIBSCORE_DO_API);
 
 api.dropletsGetAll({}, function(err, response) {
   var droplets = response.body.droplets;
+  if (response.body.links && response.body.links.pages) {
+    api.dropletsGetAll({ page: 2 }, function(err, response) {
+      droplets = droplets.concat(response.body.droplets);
+      destroy(droplets);
+    });
+  } else {
+    destroy(droplets);
+  }
+});
+
+function destroy(droplets) {
   async.eachSeries(droplets, function(droplet, callback) {
     if (/^crawler-\d+$/.test(droplet.name) && droplet.status === 'off') {
       api.dropletsDelete(droplet.id, callback);
@@ -12,4 +23,4 @@ api.dropletsGetAll({}, function(err, response) {
       callback(null);
     }
   });
-});
+}
