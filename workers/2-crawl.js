0903a587b801cb593c1864d45fcebddb2fadb61a
Jason Chen <jhchen7@gmail.com>
2015-11-03 10:57:21 -0800

    consoliate kue stuff into crawler

--- workers/2-crawl.js
+++ workers/2-crawl.js
@@ -15,6 +15,22 @@ var NUM_CRAWLERS = 32;
 var queue = kue.createQueue({
   redis: { auth: process.env.LIBSCORE_REDIS_PASS }
 });
+kue.app.set('title', 'Libscore Crawl Queue');
+kue.app.listen(3000);
+queue.watchStuckJobs();
+queue.on('job failed', function(id, result) {
+  kue.Job.get(id, function(err, job) {
+    if (err || !job) return;
+    job.data.priority += 1;
+    if (job.data.priority < 5) {
+      var newJob = queue.create('website', job.data).priority(job.data.priority).ttl(60*1000);
+      process.nextTick(function() {
+        job.remove();
+        newJob.save();
+      });
+    }
+  });
+});
 
 
 async.series([
